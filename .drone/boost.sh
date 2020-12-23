2b42c396960beac7ff2f51fff7a75cb6d72cda97
sdarwin <samuel.d.darwin@gmail.com>
2020-12-23 16:15:36 +0000

    drone

---
+++ .drone/boost.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
+export VCS_COMMIT_ID=$DRONE_COMMIT
+export GIT_COMMIT=$DRONE_COMMIT
+export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> BEFORE_INSTALL'
+
+. .drone/before-install.sh
+
+echo '==================================> INSTALL'
+
+BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
+cd ..
+git clone -b $BOOST_BRANCH https://github.com/boostorg/boost.git boost-root
+cd boost-root
+git submodule update --init tools/build
+git submodule update --init libs/config
+git submodule update --init tools/boostdep
+cp -r $TRAVIS_BUILD_DIR/* libs/xpressive
+python tools/boostdep/depinst/depinst.py xpressive
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> BEFORE_SCRIPT'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+
+echo '==================================> SCRIPT'
+
+./b2 libs/xpressive/test toolset=$TOOLSET cxxstd=$CXXSTD
+
+echo '==================================> AFTER_SUCCESS'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
